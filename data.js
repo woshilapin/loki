515c97a91e452eec533a153fac14a8f85c6049b3
github-action-benchmark <github@users.noreply.github.com>
2021-08-20 10:54:22 +0000

    add Loki Benchmark (cargo) benchmark result for 96353e1ccdcb563cd76b256ae012fb15053b5e86

---
+++ data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1629456862063,
+  "repoUrl": "https://github.com/CanalTP/loki",
+  "entries": {
+    "Loki Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "hicham.azimani@kisio.com",
+            "name": "HichamAz",
+            "username": "AzHicham"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "96353e1ccdcb563cd76b256ae012fb15053b5e86",
+          "message": "Merge pull request #58 from CanalTP/bench\n\n[CI] Benchmark",
+          "timestamp": "2021-08-20T12:46:50+02:00",
+          "tree_id": "633ac37201f376d65337c8d18310c803515ddf8e",
+          "url": "https://github.com/CanalTP/loki/commit/96353e1ccdcb563cd76b256ae012fb15053b5e86"
+        },
+        "date": 1629456860841,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "routing_loads_bench",
+            "value": 21142,
+            "range": "± 2106",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "setup_routing_basic_bench",
+            "value": 20755,
+            "range": "± 4228",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
