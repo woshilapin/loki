a62e58f507b66cc621d7bd4eb17d4598a6d22169
github-action-benchmark <github@users.noreply.github.com>
2021-09-06 15:55:43 +0000

    add Loki Benchmark (cargo) benchmark result for 21a25e961b047e1ab1dc0228e0fa25491d350144

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1630928198069,
+  "lastUpdate": 1630943743125,
   "repoUrl": "https://github.com/CanalTP/loki",
   "entries": {
     "Loki Benchmark": [
@@ -251,6 +251,42 @@ window.BENCHMARK_DATA = {
             "name": "routing_periodic_bench",
             "value": 24797,
             "range": "± 2017",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "52410095+pbench@users.noreply.github.com",
+            "name": "pbench",
+            "username": "pbench"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "21a25e961b047e1ab1dc0228e0fa25491d350144",
+          "message": "Merge pull request #64 from CanalTP/split_vj_by_timezone\n\nA new timetable implementation, where a copy of a vehicle journey is made for each day interval on which the offset from the local timezone to UTC is constant.",
+          "timestamp": "2021-09-06T17:48:20+02:00",
+          "tree_id": "e33d5b6a2df20d561b155f85aa036b2ed5510df9",
+          "url": "https://github.com/CanalTP/loki/commit/21a25e961b047e1ab1dc0228e0fa25491d350144"
+        },
+        "date": 1630943741876,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "routing_daily_bench",
+            "value": 105856,
+            "range": "± 26364",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "routing_periodic_bench",
+            "value": 21232,
+            "range": "± 2004",
             "unit": "ns/iter"
           }
         ]
